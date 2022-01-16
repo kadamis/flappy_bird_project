dce762a5e6a615ce034e4fe81653881a7c8e56bf
Konstantinos Adamis <95548427+kadamis@users.noreply.github.com>
2022-01-16 23:40:29 +0200

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+
+W, H = 800, 600
+WIN = pygame.display.set_mode((W,H))
+
+background = pygame.image.load("C:/Users/kosta/OneDrive/Υπολογιστής/Python Files/flappy_bird/images/stage.png")
+background = pygame.transform.scale(background,(W,H))
+
+game_sound = pygame.mixer.music.load("sound.wav")
+pygame.mixer.music.play(-1)
+
+def main():
+
+    run = True
+    while run:
+        WIN.blit(background,(0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        pygame.display.update()
+
+    pygame.quit()
+
+if __name__=="__main__":
+    main()
